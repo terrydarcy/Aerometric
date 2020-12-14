ff47b7d49b30b942d62182c90cc0fb9be1ca0c93
Terry D'Arcy <terrykdarcy@gmail.com>
2020-12-14 16:05:28 +0000

    flight details card

--- components/FlightDetails.js
+++ components/FlightDetails.js
@@ -1,38 +1,75 @@
-import {loadPartialConfig} from '@babel/core';
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {withOrientation} from 'react-navigation';
+import {View, Image, StyleSheet, Text} from 'react-native';
+import {
+  Avatar,
+  Button,
+  Card,
+  Title,
+  Paragraph,
+  Subheading,
+} from 'react-native-paper';
 
 const FlightDetails = ({navigation, props}) => {
-  const [title, setTitle] = useState('');
   const [flightCode, setFlightCode] = useState('');
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
   const [altitude, setAltitude] = useState('');
 
   useEffect(() => {
-    setTitle(props.title);
     setFlightCode(props.flightCode);
     setLatitude(props.latitude);
     setLongitude(props.longitude);
     setAltitude(props.altitude);
   });
+  const leftContent = () => {
+    return (
+      <Image
+        source={require('../res/plane.png')}
+        style={{
+          flex: 1,
+          width: 55,
+          height: null,
+          resizeMode: 'contain',
+        }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.detailsContainer}>
-        <Text style={[styles.text, styles.flight_title]}>
-          Flight {flightCode}
-        </Text>
-        <Text style={[styles.text]}> longitude: {longitude}</Text>
-        <Text style={[styles.text]}> latitude: {latitude}</Text>
-        <Text style={[styles.text]}> altitude: {altitude}</Text>
-        <Text style={[styles.text]}>
-          {'\n'}
-          Flight {flightCode} is cruising at an altitude {'\n'}
-          of {altitude}m, with a longitude of {longitude} {'\n'}
-          and a latitude of {latitude}.
-        </Text>
-      </View>
+      <Card style={styles.flightContainer}>
+        <Card.Title
+          style={{paddingLeft: 10}}
+          title={'Flight ' + flightCode}
+          subtitle="ICAO 24-BIT ADDRESS"
+          left={leftContent}
+        />
+        <Card.Content>
+          <Subheading>
+            Longitude:
+            <Text style={styles.apiDataTextColor}> {longitude}</Text>
+          </Subheading>
+          <Subheading>
+            Latitude:
+            <Text style={styles.apiDataTextColor}> {latitude}</Text>
+          </Subheading>
+          <Subheading>
+            Altitude:
+            <Text style={styles.apiDataTextColor}> {altitude}m</Text>
+          </Subheading>
+          <Subheading>
+            {'\n'}
+            Flight <Text style={styles.apiDataTextColor}>{flightCode}</Text> is
+            cruising at an altitude of{' '}
+            <Text style={styles.apiDataTextColor}>{altitude}m</Text>, with a
+            longitude of{' '}
+            <Text style={styles.apiDataTextColor}>{longitude}</Text> and a
+            latitude of <Text style={styles.apiDataTextColor}>{latitude}</Text>.
+          </Subheading>
+        </Card.Content>
+
+        <Card.Actions></Card.Actions>
+      </Card>
     </View>
   );
 };
@@ -42,19 +79,20 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     height: 200,
     flexDirection: 'column',
-
+    marginTop: 50,
     justifyContent: 'center',
   },
   detailsContainer: {
     backgroundColor: 'white',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    paddingHorizontal: 25,
+    paddingHorizontal: 20,
     paddingVertical: 10,
-    flex: 1,
     margin: 20,
     minHeight: 200,
-    borderRadius: 20,
+  },
+  flightContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    margin: 20,
   },
   flight_title: {
     fontSize: 20,
@@ -65,6 +103,15 @@ const styles = StyleSheet.create({
     color: 'black',
     fontFamily: 'Roboto-Regular',
     fontSize: 15,
+  },
+  imageArea: {
+    flex: 1,
+  },
+  infoArea: {
+    flex: 1,
+  },
+  apiDataTextColor: {
+    color: 'blue',
   },
 });
 
