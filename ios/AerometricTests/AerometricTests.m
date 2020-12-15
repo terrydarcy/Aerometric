eb16c5a7487b69ae08b08daec4a1166493e390a8
Terry D'Arcy <terrykdarcy@gmail.com>
2020-12-15 19:56:58 +0000

    ios

--- ios/AerometricTests/AerometricTests.m
+++ ios/AerometricTests/AerometricTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AeroAppTests : XCTestCase
+@interface AerometricTests : XCTestCase
 
 @end
 
-@implementation AeroAppTests
+@implementation AerometricTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
