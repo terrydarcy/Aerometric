4df79106919436a5f2031effa51abf64014b6186
Terry D'Arcy <terrykdarcy@gmail.com>
2021-03-17 12:16:32 +0000

    Google maps integration

--- screens/MapTab.js
+++ screens/MapTab.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const MapTab = ({}) => {
   return (
-    <MapView
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+        onPress={(e) => console.log(e.nativeEvent)}></MapView>
+    </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    ...StyleSheet.absoluteFillObject,
+
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
